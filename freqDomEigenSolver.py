9684d4c78ec37153c11c25358bd3be000bc5bd65
justinbrown <jbro4777@uni.sydney.edu.au>
2021-03-25 15:44:56 +1100

    increased res

--- freqDomEigenSolver.py
+++ freqDomEigenSolver.py
@@ -2,16 +2,13 @@
 
 import meep as mp
 import numpy as np
-import matplotlib.pyplot as plt
+
+# import matplotlib.pyplot as plt
 
 # Characteristic lengthscale
 a = 1  # micrometers
 
-resolution = 20  # pixels/um
-
-sx = 20  # size of cell in X direction
-sy = 20  # size of cell in Y direction
-cell = mp.Vector3(sx, sy, 0)
+resolution = 10  # pixels/um
 
 # Creating perfectly matched layers
 dpml = 1.0
@@ -27,29 +24,39 @@ realPerm = 12
 cond = 2 * np.pi * fcen * complexPerm / realPerm
 
 # Some integer
-m = 30
+m = 10
 
 # Size of resonator and waveguide
-r = m * wvln / (2 * np.pi)
+r = m*wvln/(2*np.pi*np.sqrt(12))
 sep = 0.1
 w = 0.1
 pad = 2
+
+sx = 2*(r+pad+dpml) # size of cell in X direction
+sy = sx  # size of cell in Y direction
+cell = mp.Vector3(sx, sy, 0)
 
 geometry = [mp.Cylinder(material=mp.Medium(epsilon=12, D_conductivity=cond),
                         center=mp.Vector3(),
                         radius=r,
                         height=mp.inf),  # Whispering gallery resonator
-            mp.Block(mp.Vector3(w, mp.inf, mp.inf),
-                     material=mp.Medium(epsilon=12),
-                     center=mp.Vector3(-(r + sep + w / 2), 0, 0)
-                     )
+            # mp.Block(mp.Vector3(w, mp.inf, mp.inf),
+            #         material=mp.Medium(epsilon=12),
+            #         center=mp.Vector3(-(r + sep + w / 2), 0, 0)
+            #         )
             ]
 
-# Scattering source
+# Waveguide Source
+# sources = [mp.Source(mp.ContinuousSource(frequency =f cen),
+#                     component = mp.Ez,
+#                     center = mp.Vector3(-(r + sep + w / 2), -sy / 2 + pad),
+#                     size = mp.Vector3(w, 0))
+#           ]
+
+# Scattering Source in resonator
 sources = [mp.Source(mp.ContinuousSource(frequency=fcen),
                      component=mp.Ez,
-                     center=mp.Vector3(-(r + sep + w / 2), -sy / 2 + pad),
-                     size=mp.Vector3(w, 0))
+                     center=mp.Vector3(-r + 0.1, 0))
            ]
 
 sim = mp.Simulation(cell_size=cell,
@@ -63,10 +70,13 @@ sim = mp.Simulation(cell_size=cell,
 
 tol = 1e-7
 maxiters = 100
-cwtol = tol*1e-3
+cwtol = tol * 1e-3
 cwmaxiters = 10000
 L = 10
 
 sim.init_sim()
 eigfreq = sim.solve_eigfreq(tol, maxiters, fcen, cwtol, cwmaxiters, L)
 Q = eigfreq.real / (-2 * eigfreq.imag)
+
+print(eigfreq)
+print(Q)
