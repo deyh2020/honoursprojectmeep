7ba49fd3b51a57f960bec3c4603e3bc93dcd4612
justinbrown <jbro4777@uni.sydney.edu.au>
2021-04-08 18:09:07 +1000

    Changed dependiency of complex permitivity

--- schillersApproxTest.py
+++ schillersApproxTest.py
@@ -26,9 +26,14 @@ print("Frequency = " + str(fcen))
 # cen = 0.6496971259430409
 df = 0.002
 
+n = 3.4483 + 1j * 1.0901e-13
+
 # Setting real and complex permitivities, and material loss
-complexPerm = 0
-realPerm = 12
+# complexPerm = 0
+# realPerm = 12
+perm = np.power(n, 2)
+complexPerm = np.imag(perm)
+realPerm = np.real(perm)
 cond = 2 * np.pi * freq * complexPerm / realPerm
 
 # Refractive index and ratio with index outside resonator
@@ -37,17 +42,17 @@ q = n / 1
 
 # Angular mode number
 # Radial mode number
-m = 30
+m = 20
 nrad = 1  # Set to 1 to get fundamental whispering gallery modes
 
 # Which zero of the airy function we need
 azs = special.ai_zeros(nrad)[0]
-az = azs[nrad-1]
+az = azs[nrad - 1]
 
 # Size of resonator and waveguide, using schillers approx
 r = 1 / (2 * np.pi * freq) * (m + 1 / 2 + az * ((m + 1) / 3) ** (1 / 3)
                               - q / np.sqrt(q ** 2 - 1) + 3 * az / (2 ** (2 / 3) * 10 * (m + 1 / 2) ** (1 / 3))
-                              + q ** 3 * az / (3 * 2**(1/3) * (q**2-1)**(3/2) * (m+1/2) ** (3/2)))
+                              + q ** 3 * az / (3 * 2 ** (1 / 3) * (q ** 2 - 1) ** (3 / 2) * (m + 1 / 2) ** (3 / 2)))
 print("Radius = " + str(r))
 sep = 0.1
 w = 0.1
@@ -61,14 +66,14 @@ geometry = [mp.Cylinder(material=mp.Medium(epsilon=12, D_conductivity=cond),
                         center=mp.Vector3(),
                         radius=r,
                         height=mp.inf),  # Whispering gallery resonator
-            #mp.Block(mp.Vector3(w, mp.inf, mp.inf),
+            # mp.Block(mp.Vector3(w, mp.inf, mp.inf),
             #         material=mp.Medium(epsilon=12),
             #         center=mp.Vector3(-(r + sep + w / 2), 0, 0)
             #         )
             ]
 
 # Waveguide Source
-#sources = [mp.Source(mp.GaussianSource(frequency=fcen, width=df),
+# sources = [mp.Source(mp.GaussianSource(frequency=fcen, width=df),
 #                     component=mp.Ez,
 #                     center=mp.Vector3(-(r + sep + w / 2), -sy / 2 + pad),
 #                     size=mp.Vector3(w, 0))
@@ -85,7 +90,9 @@ sim = mp.Simulation(cell_size=cell,
                     geometry=geometry,
                     sources=sources,
                     resolution=resolution,
-                    force_complex_fields=False)
+                    force_complex_fields=False,
+                    Courant=0.5
+                    )
 
 sim.use_output_directory()
 h = mp.Harminv(mp.Ez, mp.Vector3(- r + 0.1), fcen, df)
